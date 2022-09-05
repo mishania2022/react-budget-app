739921a8c31060ec53c956e1ab7dd4111a4738e4
mikhail <VO3@Pgasn>
2022-09-05 12:42:33 +0300

    feat:add hooks

---
+++ src/hooks/useToggle.tsx
@@ -0,0 +1,11 @@
+import { useCallback, useState } from "react";
+
+export const useToggle = (
+  initialValue: boolean = false
+): [boolean, () => void] => {
+  const [state, setState] = useState<boolean>(initialValue);
+
+  const toggle = useCallback((): void => setState((state) => !state), []);
+
+  return [state, toggle];
+};
